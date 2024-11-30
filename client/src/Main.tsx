c6fb4c3f60ae06fbadc741e84527de73eacd0ec0
mannco1 <f.mannco@yandex.ru>
2024-11-30 18:23:25 +0300

    shutter

--- client/src/Main.tsx
+++ client/src/Main.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
 import './Main.css'
+import Shutter from './components/Shutter';
+
 
 const Main = () => {
+    const subdivisions = ["Подразделение 1", "Подразделение 2", "Подразделение 3"];
     return (
         <div className='main-container'>
             <Header/>
+            <div className='main-content'>
+                 
+            <Shutter/>
+           </div>
         </div>
     );
 };
